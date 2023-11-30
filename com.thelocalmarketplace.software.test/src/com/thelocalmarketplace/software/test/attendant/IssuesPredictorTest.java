f68710a665a990343f2d96beaeb8a3047a5c2d05
arcleah <arcleahpascual@gmail.com>
2023-11-30 15:46:00 -0700

    in progress!

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/attendant/IssuesPredictorTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/attendant/IssuesPredictorTest.java
@@ -1,14 +1,26 @@
 package com.thelocalmarketplace.software.test.attendant;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
 
+import com.jjjwelectronics.IDevice;
 import com.jjjwelectronics.Numeral;
+import com.jjjwelectronics.OverloadedDevice;
 import com.jjjwelectronics.printer.IReceiptPrinter;
+import com.jjjwelectronics.printer.ReceiptPrinterBronze;
+import com.jjjwelectronics.printer.ReceiptPrinterGold;
+import com.jjjwelectronics.printer.ReceiptPrinterListener;
+import com.jjjwelectronics.printer.ReceiptPrinterSilver;
 import com.jjjwelectronics.scale.IElectronicScale;
 import com.jjjwelectronics.scanner.Barcode;
 import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
 import com.thelocalmarketplace.hardware.BarcodedProduct;
+import com.thelocalmarketplace.hardware.SelfCheckoutStationBronze;
+import com.thelocalmarketplace.hardware.SelfCheckoutStationGold;
 import com.thelocalmarketplace.hardware.SelfCheckoutStationSilver;
 import com.thelocalmarketplace.software.Session;
 import com.thelocalmarketplace.software.SessionState;
@@ -21,8 +33,8 @@ import com.thelocalmarketplace.software.weight.Weight;
 
 import powerutility.PowerGrid;
 
-public class IssuesPredictorTest{
-
+public class IssuesPredictorTest extends AbstractTest{
+	
 	private Session session;
 	byte num;
 	private Numeral numeral;
@@ -38,22 +50,26 @@ public class IssuesPredictorTest{
 	private IssuePredictor issuePredictor;
 	private SelfCheckoutStationSilver scss;
 	private PowerGrid powerGrid;
+	private SelfCheckoutStationGold scsg;
+	private IReceiptPrinter printer;
+	private ReceiptPrinterSilver silverPrinter;
+	private ReceiptPrinterBronze bronzePrinter;
+	private ReceiptPrinterGold goldPrinter;
 
-//	public IssuesPredictorTest(String testName, AbstractSelfCheckoutStation scs) {
-//		super(testName, scs);
-//	}
+	public IssuesPredictorTest(String testName, AbstractSelfCheckoutStation scs) {
+		super(testName, scs);
+	}
+	
 	
 	@Before
 	public void setUp() {
-		AbstractSelfCheckoutStation.resetConfigurationToDefaults();
-    	PowerGrid.engageUninterruptiblePowerSource();
+		basicDefaultSetup();
+		
+		// Create power source
+		PowerGrid.engageUninterruptiblePowerSource();
     	powerGrid = PowerGrid.instance();
-    	scss = new SelfCheckoutStationSilver();
     	
-    	scss.plugIn(powerGrid);
-		scss.turnOn();
-		
-		
+    	// Set up session
 		session = new Session();
         num = 1;
         numeral = Numeral.valueOf(num);
@@ -62,24 +78,81 @@ public class IssuesPredictorTest{
         barcode2 = new Barcode(new Numeral[] { numeral });
         product = new BarcodedProduct(barcode, "Sample Product", 10, 100.0);
         product2 = new BarcodedProduct(barcode2, "Sample Product 2", 15, 20.0);
-        funds = new Funds(scss);
+        funds = new Funds(scs);
         itemManager = new ItemManager(session);
 
-        IElectronicScale baggingArea = scss.getBaggingArea();
+        IElectronicScale baggingArea = scs.getBaggingArea();
         weight = new Weight(baggingArea);
-
-        IReceiptPrinter printer = scss.getPrinter();
+        
+        printer = scs.getPrinter();
         receiptPrinter = new Receipt(printer);
         issuePredictor = new IssuePredictor();
+       
+        // Bronze Printer
+        bronzePrinter = new ReceiptPrinterBronze();
+        bronzePrinter.plugIn(powerGrid);
+		bronzePrinter.turnOn();
+		
+        // Silver Printer
+		silverPrinter = new ReceiptPrinterSilver();
+        silverPrinter.plugIn(powerGrid);
+		silverPrinter.turnOn();
+		
+		// Gold Printer
+		goldPrinter = new ReceiptPrinterGold();
+        goldPrinter.plugIn(powerGrid);
+		goldPrinter.turnOn();
 	}
 	
 	@Test 
-	public void testCheckLowInk() {
-		session.setup(itemManager, funds, weight, receiptPrinter, scss);
-		//issuePredictor.checkLowInk(session);
-		System.out.println(scss.getPrinter());
-		
-		
+	public void testCheckLowInk() throws OverloadedDevice {
+		session.setup(itemManager, funds, weight, receiptPrinter, scs);
+		// Bronze
+		issuePredictor.checkLowInk(session, scs.getPrinter());
+		// Silver
+		silverPrinter.addInk(100000);
+		issuePredictor.checkLowInk(session, silverPrinter);
+		// Gold
+		goldPrinter.addInk(100000);
+		issuePredictor.checkLowInk(session, goldPrinter);
 	}
 	
+	@Test
+	public void testCheckLowInkWhenFull() throws OverloadedDevice {
+		session.setup(itemManager, funds, weight, receiptPrinter, scs);
+		// Bronze
+		issuePredictor.checkLowInk(session, scs.getPrinter());
+		// Silver
+		silverPrinter.addInk(1 << 20);
+		issuePredictor.checkLowInk(session, silverPrinter);
+		// Gold
+		goldPrinter.addInk(1 << 20);
+		issuePredictor.checkLowInk(session, goldPrinter);
+	}
+	
+	@Test
+	public void testCheckLowPaper() throws OverloadedDevice {
+		session.setup(itemManager, funds, weight, receiptPrinter, scs);
+		// Bronze
+		issuePredictor.checkLowPaper(session, scs.getPrinter());
+		// Silver
+		silverPrinter.addPaper(100);
+		issuePredictor.checkLowPaper(session, silverPrinter);
+		// Gold
+		goldPrinter.addPaper(100);
+		issuePredictor.checkLowPaper(session, goldPrinter);
+	}
+	
+	@Test
+	public void testCheckLowPaperWhenFull() throws OverloadedDevice {
+		session.setup(itemManager, funds, weight, receiptPrinter, scs);
+		// Bronze
+		issuePredictor.checkLowPaper(session, scs.getPrinter());
+		// Silver
+		silverPrinter.addPaper(1 << 10);
+		issuePredictor.checkLowPaper(session, silverPrinter);
+		// Gold
+		goldPrinter.addPaper(1 << 10);
+		issuePredictor.checkLowPaper(session, goldPrinter);
+	}
 }
