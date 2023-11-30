289cdc675e3ef1c651ca252abaedec7e777c5074
junheoucal <junbeom.heo@ucalgary.ca>
2023-11-29 21:14:40 -0700

    Bugfixes to coin and banknote removal

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/attendant/MaintenanceManager.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/attendant/MaintenanceManager.java
@@ -4,8 +4,10 @@ import com.jjjwelectronics.OverloadedDevice;
 import com.jjjwelectronics.printer.IReceiptPrinter;
 import com.tdc.CashOverloadException;
 import com.tdc.banknote.Banknote;
+import com.tdc.banknote.BanknoteStorageUnit;
 import com.tdc.banknote.IBanknoteDispenser;
 import com.tdc.coin.Coin;
+import com.tdc.coin.CoinStorageUnit;
 import com.tdc.coin.ICoinDispenser;
 import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
 import com.thelocalmarketplace.software.Session;
@@ -52,8 +54,8 @@ public class MaintenanceManager {
     private Session session;
     private BigDecimal[] banknoteDenominations;
     private List<BigDecimal> coinDenominations;
-    private ICoinDispenser openCoinDispenser;
-    private IBanknoteDispenser openBanknoteDispenser;
+    private BanknoteStorageUnit banknoteStorage;
+    private CoinStorageUnit coinStorage;
 
     /**
      * Exception class - Thrown when the attendant tries to open the hardware when it is not disabled
@@ -90,6 +92,8 @@ public class MaintenanceManager {
             receiptPrinter = scs.getPrinter();
             banknoteDispensers = scs.getBanknoteDispensers();
             coinDispensers = scs.getCoinDispensers();
+            banknoteStorage = scs.getBanknoteStorage();
+            coinStorage = scs.getCoinStorage();
         }
         else {
             throw new NotDisabledSessionException("Session is not disabled!");
@@ -117,15 +121,11 @@ public class MaintenanceManager {
     }
 
     /**
-     * Simulates removing coins from selected Banknote Dispenser
-     * @param cd the denomination of the Banknote Dispenser
+     * Simulates removing coins from Coin Storage
      * @return the coins that are removed
      */
-    public List<Coin> removeCoins(BigDecimal cd) {
-        if (coinDenominations.contains(cd)) {
-            return coinDispensers.get(cd).unload();
-        }
-        return null;
+    public List<Coin> removeCoins() {
+        return coinStorage.unload();
     }
 
     /**
@@ -164,15 +164,11 @@ public class MaintenanceManager {
     }
 
     /**
-     * Simulates removing banknotes from a specified Banknote Dispenser
-     * @param bd the denomination of the specified Banknote Dispenser
+     * Simulates removing banknotes from Banknote Storage
      * @return the removed banknotes
      */
-    public List<Banknote> removeBanknotes(BigDecimal bd) {
-        if (verifyBanknoteDenomination(bd)) {
-            return banknoteDispensers.get(bd).unload();
-        }
-        return null;
+    public List<Banknote> removeBanknotes() {
+        return banknoteStorage.unload();
     }
 
     /**
