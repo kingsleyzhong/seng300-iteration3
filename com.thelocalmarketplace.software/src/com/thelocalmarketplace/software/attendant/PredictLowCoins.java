80aaca142da7328609f46327c70f3f9fe43eef8d
arcleah <arcciepasc@gmail.com>
2023-11-29 20:46:25 -0700

    Update PredictLowCoins.java

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/attendant/PredictLowCoins.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/attendant/PredictLowCoins.java
@@ -1,16 +1,14 @@
 package com.thelocalmarketplace.software.attendant;
 
-import com.tdc.coin.CoinStorageUnit;
 import com.thelocalmarketplace.software.*;
 
 public class PredictLowCoins extends AbstractPredictIssue {
-	private CoinStorageUnit coinStorageUnit;
 	private Session s;
 	
 	public void checkLowCoins() {
 		if (s.getState() == SessionState.PRE_SESSION) {
-			int currentCoins = coinStorageUnit.getCoinCount();
-			int maxCoins = coinStorageUnit.getCapacity();
+			int currentCoins = s.getStation().getCoinStorage().getCoinCount();
+			int maxCoins = s.getStation().getCoinStorage().getCapacity();
 			
 			if (currentCoins <= maxCoins * 0.1) { // if current coins is less than 10% of its maxCoins
 				signalAttendant("Low on Coins");
