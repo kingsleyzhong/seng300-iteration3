8f323806a0cdf1fbf6d1710b578e30410cd64675
Ekkiddle <ekkiddle@gmail.com>
2023-12-01 13:19:54 -0700

    Large reformatting fixes. Start screen is now tied to session gui

--- com.thelocalmarketplace.software/GUI/com/thelocalmarketplace/GUI/Main.java
+++ com.thelocalmarketplace.software/GUI/com/thelocalmarketplace/GUI/Main.java
@@ -5,7 +5,7 @@ import com.thelocalmarketplace.GUI.startscreen.StartScreenGUI;
 public class Main {
 	
 	public static void main(String[] args) {
-		new StartScreenGUI();
+		//new StartScreenGUI();
 		new Simulation();
 	}
 
--- com.thelocalmarketplace.software/GUI/com/thelocalmarketplace/GUI/session/SoftwareGUI.java
+++ com.thelocalmarketplace.software/GUI/com/thelocalmarketplace/GUI/session/SoftwareGUI.java
@@ -25,6 +25,7 @@ import com.jjjwelectronics.Mass;
 import com.jjjwelectronics.screen.ITouchScreen;
 import com.thelocalmarketplace.GUI.Simulation;
 import com.thelocalmarketplace.GUI.customComponents.Colors;
+import com.thelocalmarketplace.GUI.customComponents.GradientPanel;
 import com.thelocalmarketplace.GUI.customComponents.PlainButton;
 import com.thelocalmarketplace.GUI.hardware.HardwareGUI;
 import com.thelocalmarketplace.GUI.startscreen.StartScreenGUI;
@@ -38,6 +39,10 @@ import javax.swing.SwingConstants;
 public class SoftwareGUI{
 	JFrame frame;
 	JFrame catalogue;
+	JPanel mainPane;
+	JPanel startPane;
+	JPanel endPane;
+	
 	Session session;
 	
 	// Buttons for user to interact with:
@@ -66,6 +71,7 @@ public class SoftwareGUI{
 	// Buttons
 	public SoftwareGUI(Session session) {
 		session.register(new InnerListener());
+		this.session = session;
 		frame = session.getStation().getScreen().getFrame();
 		catalogue = new SearchCatalogue(session);
 		
@@ -79,23 +85,67 @@ public class SoftwareGUI{
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.setVisible(true);
 		
+		mainPane = mainPanel();
+		startPane = start();
+		//endPane = end();
+		
+		displayStart();
+	}
+
+	public JPanel start() {
+		JPanel main = new GradientPanel(Colors.color2, Colors.color1);
+		main.setLayout(new GridLayout(0,1,50,50));
+		
+		//this is the start button
+		JButton btnStart = new PlainButton("Start", Colors.color1);
+		btnStart.setOpaque(false);
+		btnStart.setFont(new Font("Dialog", Font.BOLD, 40));
+		btnStart.setBorder(BorderFactory.createEmptyBorder());
+		btnStart.setMaximumSize(new Dimension(200,100));
+		btnStart.setForeground(Color.BLACK);
+		btnStart.setBackground(Colors.color1);
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				session.start();
+				displayMain();
+			}
+		});
+		
+		
+		
+		JLabel lblWelcome = new JLabel("WELCOME");
+		lblWelcome.setBackground(Color.RED);
+		lblWelcome.setForeground(Colors.color3);
+		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcome.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblWelcome.setFont(new Font("Dialog", Font.BOLD, 92));
+		main.add(lblWelcome, JLabel.CENTER_ALIGNMENT);
+		main.add(btnStart, JButton.CENTER_ALIGNMENT);	
+		
+		return main;
+	}
+	
+	
+	public JPanel mainPanel() {
+		JPanel main = new JPanel();
+		main.setLayout(new BorderLayout());
 		
 		// ORANGE PANEL FOR ITEM DETAILS:
 		orangePanel = new JPanel();
 		orangePanel.setBackground(Colors.color5);
 		orangePanel.setLayout(new BorderLayout());
-		
+				
 		JButton hardwareButton = new PlainButton("Hardware GUI", Colors.color5);
 		hardwareButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				HardwareGUI.setVisibility(true);
-				
+					
 			}
-			
+					
 		});
-		
+				
 		JPanel oLeft = new JPanel();
 		oLeft.setBackground(Colors.color5);
 		oLeft.setLayout(new FlowLayout());
@@ -138,7 +188,6 @@ public class SoftwareGUI{
 		infoTop.setPreferredSize(new Dimension(width, 300));
 		infoTop.setLayout(new GridLayout(0,1,20,20));
 		infoTop.setBorder(BorderFactory.createEmptyBorder(20,50,20,50));
-	
 		JPanel infoTop1 = new JPanel();
 		infoTop1.setLayout(new BorderLayout());
 		infoTop1.setBackground(Colors.color4);
@@ -149,7 +198,7 @@ public class SoftwareGUI{
 		infoTop1.setLayout(new BorderLayout(0, 0));
 		infoTop1.add(infoQtyString, BorderLayout.WEST);
 		infoTop1.add(infoQtyNumber, BorderLayout.EAST);
-		
+				
 		JPanel infoTop2 = new JPanel();
 		infoTop2.setBackground(Colors.color4);
 		infoTop2.setLayout(new BorderLayout());
@@ -160,7 +209,7 @@ public class SoftwareGUI{
 		infoItemNumber.setFont(new Font("Dialog", Font.BOLD,20));
 		infoTop2.add(infoItemString, BorderLayout.WEST);
 		infoTop2.add(infoItemNumber, BorderLayout.EAST);
-		
+				
 		JPanel infoTop3 = new JPanel();
 		infoTop3.setBackground(Colors.color4);
 		infoTop3.setLayout(new BorderLayout());
@@ -171,43 +220,43 @@ public class SoftwareGUI{
 		infoWeightNumber.setFont(new Font("Dialog", Font.BOLD,20));
 		infoTop3.add(infoWeightString, BorderLayout.WEST);
 		infoTop3.add(infoWeightNumber, BorderLayout.EAST);
-		
+				
 		infoTop.add(infoTop1);
 		infoTop.add(infoTop2);
 		infoTop.add(infoTop3);
-		
+			
 		JPanel infoBottom = new JPanel();
 		infoBottom.setBackground(Colors.color4);
 		infoBottom.setLayout(new GridLayout(0,1));
 		infoBottom.setBorder(BorderFactory.createMatteBorder(3, 0, 0, 0, Colors.color1));
-		
+				
 		JPanel infoBottomInner = new JPanel();
 		infoBottomInner.setLayout(new BorderLayout());
 		infoBottomInner.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
 		infoBottomInner.setBackground(Colors.color4);
-		
+				
 		JLabel cartTotalString = new JLabel("Cart Total");
 		cartTotalString.setFont(new Font("Dialog", Font.BOLD, 20));
 		JLabel cartTotalInDollars = new JLabel("$0.00");
 		cartTotalInDollars.setFont(new Font("Dialog", Font.BOLD,40));
-		
+			
 		infoBottomInner.add(cartTotalString, BorderLayout.WEST);
 		infoBottomInner.add(cartTotalInDollars, BorderLayout.EAST);
-		
+				
 		infoBottom.add(infoBottomInner);
-		
+			
 		cartInfoPanel.add(infoTop, BorderLayout.NORTH);
 		cartInfoPanel.add(infoBottom, BorderLayout.SOUTH);
-		
+				
 		cartItemsPanel = new AddedProducts(session);
 		//cartItemsPanel.setBackground(Colors.color3);
 		cartItemsPanel.setPreferredSize(new Dimension( 2 * (width/3), height));
-		
+				
 		// BUTTON PANEL FOR BOTTOM OF SCREEN:
 		buttonPanel = new JPanel();
 		buttonPanel.setBackground(Colors.color2);
 		buttonPanel.setLayout(new GridLayout(1,0, 20,0));
-		
+			
 		// Create buttons
 		exit = new PlainButton("Exit session", Colors.color4);
 		addBags = new PlainButton("Add Bags", Colors.color4);
@@ -215,22 +264,22 @@ public class SoftwareGUI{
 		searchCatalogue = new PlainButton("<html>&nbsp;&nbsp;Search <br/>Catalogue</html>", Colors.color4);
 		callAttendant = new PlainButton("<html>&nbsp;&nbsp;&nbsp;Call <br/>Attendant</html>", Colors.color4);
 		pay = new PlainButton("PAY", Colors.color4);
-	
+		
 		//Add action listeners to buttons
 		exit.addActionListener(new ButtonListener());
 		addBags.addActionListener(new ButtonListener());
 		pluCode.addActionListener(new ButtonListener());
 		searchCatalogue.addActionListener(new ButtonListener());
 		callAttendant.addActionListener(new ButtonListener());
-		
-        paymentScreen = new PaymentPopup(session);
+				
+		paymentScreen = new PaymentPopup(this.session);
 		pay.addActionListener(new ButtonListener(){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                paymentScreen.popUp();
-                       }
-        });
-		
+		      @Override
+		       public void actionPerformed(ActionEvent e) {
+		            paymentScreen.popUp();
+		       }
+		});
+				
 		//Set button sizes
 		exit.setFont(new Font("Dialog", Font.BOLD, 30));
 		addBags.setFont(new Font("Dialog", Font.BOLD, 30));
@@ -238,7 +287,7 @@ public class SoftwareGUI{
 		searchCatalogue.setFont(new Font("Dialog", Font.BOLD, 30));
 		callAttendant.setFont(new Font("Dialog", Font.BOLD, 30));
 		pay.setFont(new Font("Dialog", Font.BOLD, 75));
-		
+			
 		// Align texts for buttons
 		exit.setHorizontalTextPosition(JButton.CENTER);
 		addBags.setHorizontalTextPosition(JButton.CENTER);		
@@ -246,7 +295,7 @@ public class SoftwareGUI{
 		searchCatalogue.setHorizontalTextPosition(JButton.CENTER);		
 		callAttendant.setHorizontalTextPosition(JButton.CENTER);		
 		pay.setHorizontalTextPosition(JButton.CENTER);		
-		
+			
 		buttonPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 		buttonPanel.add(exit);
 		buttonPanel.add(addBags);
@@ -254,19 +303,41 @@ public class SoftwareGUI{
 		buttonPanel.add(searchCatalogue);
 		buttonPanel.add(callAttendant);
 		buttonPanel.add(pay);
-		
-		
+			
+				
 		// SET PANEL SIZES FOR JFRAME:
 		orangePanel.setPreferredSize(new Dimension(width, height/13));
 		buttonPanel.setPreferredSize(new Dimension(width, height/6));
-		
+			
 		// ADD PANELS TO JFRAME:
-		frame.getContentPane().add(orangePanel, BorderLayout.NORTH);
-		frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
-		frame.getContentPane().add(cartInfoPanel, BorderLayout.WEST);
-		frame.getContentPane().add(cartItemsPanel, BorderLayout.EAST);
+		main.add(orangePanel, BorderLayout.NORTH);
+		main.add(buttonPanel, BorderLayout.SOUTH);
+		main.add(cartInfoPanel, BorderLayout.WEST);
+		main.add(cartItemsPanel, BorderLayout.EAST);
+		
+		return main;
 	}
-
+	
+	public void displayStart() {
+		frame.getContentPane().removeAll();
+		frame.getContentPane().add(startPane, BorderLayout.CENTER);
+		frame.getContentPane().revalidate();
+		frame.getContentPane().repaint();
+	}
+	
+	public void displayMain() {
+		frame.getContentPane().removeAll();
+		frame.getContentPane().add(mainPane, BorderLayout.CENTER);
+		frame.getContentPane().revalidate();
+		frame.getContentPane().repaint();
+	}
+	
+	public void displayEnd() {
+		frame.getContentPane().removeAll();
+		frame.getContentPane().add(endPane);
+		frame.getContentPane().revalidate();
+		frame.getContentPane().repaint();
+	}
 	
 	public void hide() {
     	frame.setVisible(false);
