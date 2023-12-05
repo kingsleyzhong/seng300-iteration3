bd45c66127f2262ad57a3d90c7906462e86914c1
ethanw <ethansfwoo@gmail.com>
2023-12-04 19:39:56 -0700

    added documentation and names/UCIDs to MemberTest

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/membership/MemberTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/membership/MemberTest.java
@@ -1,4 +1,30 @@
 package com.thelocalmarketplace.software.test.membership;
+/* Tests related to the Member class and the MembershipDatabase
+ *
+ * Project Iteration 3 Group 1
+ *
+ * Derek Atabayev 			: 30177060
+ * Enioluwafe Balogun 		: 30174298
+ * Subeg Chahal 			: 30196531
+ * Jun Heo 					: 30173430
+ * Emily Kiddle 			: 30122331
+ * Anthony Kostal-Vazquez 	: 30048301
+ * Jessica Li 				: 30180801
+ * Sua Lim 					: 30177039
+ * Savitur Maharaj 			: 30152888
+ * Nick McCamis 			: 30192610
+ * Ethan McCorquodale 		: 30125353
+ * Katelan Ng 				: 30144672
+ * Arcleah Pascual 			: 30056034
+ * Dvij Raval 				: 30024340
+ * Chloe Robitaille 		: 30022887
+ * Danissa Sandykbayeva 	: 30200531
+ * Emily Stein 				: 30149842
+ * Thi My Tuyen Tran 		: 30193980
+ * Aoi Ueki 				: 30179305
+ * Ethan Woo 				: 30172855
+ * Kingsley Zhong 			: 30197260
+ */
 
 import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
 import com.thelocalmarketplace.software.membership.Member;
@@ -16,12 +42,14 @@ public class MemberTest extends AbstractSessionTest {
         super(testName, scsClass);
     }
 
+    /** Clear the Membership database and re-register the test member*/
     @Before
     public void setup() {
         MembershipDatabase.MEMBERSHIP_DATABASE.clear();
         MembershipDatabase.registerMember(membershipNumber, memberName);
     }
 
+    /** A newly registered member should be present in the database. */
     @Test
     public void testRegisterMember() {
         String membershipNumber2 = "123";
@@ -30,11 +58,13 @@ public class MemberTest extends AbstractSessionTest {
         Assert.assertTrue(MembershipDatabase.MEMBERSHIP_DATABASE.containsKey(membershipNumber2));
     }
 
+    /** A member object in the database should return the correct corresponding name. */
     @Test
     public void testMemberName() {
         Assert.assertEquals(memberName, MembershipDatabase.MEMBERSHIP_DATABASE.get(membershipNumber).getName());
     }
 
+    /** Points should go up with a positive delta. */
     @Test
     public void testAddPoints() {
         Member member = MembershipDatabase.MEMBERSHIP_DATABASE.get(membershipNumber);
@@ -42,6 +72,7 @@ public class MemberTest extends AbstractSessionTest {
         Assert.assertEquals(10, member.getPoints());
     }
 
+    /** Points should go down with a negative delta. */
     @Test
     public void testSubtractPoints() {
         Member member = MembershipDatabase.MEMBERSHIP_DATABASE.get(membershipNumber);
@@ -50,6 +81,7 @@ public class MemberTest extends AbstractSessionTest {
         Assert.assertEquals(5, member.getPoints());
     }
 
+    /** Negative points should not be possible. */
     @Test
     public void testNegativePoints() {
         Member member = MembershipDatabase.MEMBERSHIP_DATABASE.get(membershipNumber);
