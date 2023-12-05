9c24adf9d0523c04fc12e1bf833da4cf6970e46f
Kingsley Zhong <kingsleyzhonghome@gmail.com>
2023-12-05 00:00:23 -0700

    Fixes and cleans

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SelfCheckoutStationSystemTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SelfCheckoutStationSystemTest.java
@@ -243,7 +243,7 @@ public class SelfCheckoutStationSystemTest extends AbstractTest {
 		}
 		Funds funds = session.getFunds();
 		assertEquals("Session is fully paid for", BigDecimal.ZERO, funds.getAmountDue());
-		assertEquals("Session has been notified of full payment", session.getState(), SessionState.PRE_SESSION);
+		assertEquals("Session has been notified of full payment", session.getState(), SessionState.DISABLED);
 	}
 
 	@Test
@@ -266,9 +266,9 @@ public class SelfCheckoutStationSystemTest extends AbstractTest {
 			}
 		}
 		Funds funds = session.getFunds();
-		//assertEquals("Paid $20", new BigDecimal(20), funds.getPaid());
+		// assertEquals("Paid $20", new BigDecimal(20), funds.getPaid());
 		assertTrue("Session is fully paid for", funds.getAmountDue().compareTo(BigDecimal.ZERO) < 0);
-		assertEquals("Session has been notified of full payment", SessionState.PRE_SESSION, session.getState());
+		assertEquals("Session has been notified of full payment", SessionState.DISABLED, session.getState());
 
 		assertTrue(scs.getBanknoteOutput().hasDanglingBanknotes());
 		List<Banknote> change = scs.getBanknoteOutput().removeDanglingBanknotes();
@@ -303,9 +303,9 @@ public class SelfCheckoutStationSystemTest extends AbstractTest {
 			}
 		}
 		Funds funds = session.getFunds();
-		//assertEquals("Paid $20", new BigDecimal(20), funds.getPaid());
+		// assertEquals("Paid $20", new BigDecimal(20), funds.getPaid());
 		assertTrue("Session is fully paid for", funds.getAmountDue().compareTo(BigDecimal.ZERO) < 0);
-		assertEquals("Session has been notified of full payment", SessionState.PRE_SESSION, session.getState());
+		assertEquals("Session has been notified of full payment", SessionState.DISABLED, session.getState());
 
 		List<Coin> coinchange = scs.getCoinTray().collectCoins();
 		List<Coin> expected = new ArrayList<>();
@@ -320,118 +320,115 @@ public class SelfCheckoutStationSystemTest extends AbstractTest {
 
 	// Tests for paying Credit via swipe
 	@Test
-	 public void payWithCredit() throws CashOverloadException,
-	 NoCashAvailableException, DisabledException, IOException {
-	 CardIssuer ci1 = new CardIssuer(SupportedCardIssuers.ONE.getIssuer(), 1);
-	 CardIssuerDatabase.CARD_ISSUER_DATABASE.put(SupportedCardIssuers.ONE.
-	 getIssuer(), ci1);
-	 Card creditCard = new Card(SupportedCardIssuers.ONE.getIssuer(),
-	 "5299334598001547", "Brandon Chan", "666", null, false, false);
-	 
-	 Calendar exp = Calendar.getInstance();
-	 exp.set(Calendar.YEAR, 2099);
-	 exp.set(Calendar.MONTH, 12);
-	 
-	 ci1.addCardData(creditCard.number, creditCard.cardholder, exp,
-	 creditCard.cvv, 10000);
-	 
-	 session.start();
-	 for(int i =0; i < 100; i++) {
-	 scs.getMainScanner().scan(item);
-	 }
-	 scs.getBaggingArea().addAnItem(item);
-	 session.payByCard();
-	 
-	 Funds funds = session.getFunds();
-	 
-	 assertEquals(SessionState.PAY_BY_CARD, session.getState());
-	 
-	 boolean read = false;
-	 while (!read) {
-	 try {
-	 scs.getCardReader().swipe(creditCard);
-	 read = true;
-	 } catch (MagneticStripeFailureException e) {
-	 }
-	 }
-	 assertEquals("Session is fully paid for", BigDecimal.ZERO,
-	 funds.getAmountDue());
-	 assertEquals("Session has been notified of full payment", session.getState(),
-	 SessionState.PRE_SESSION);
-	 }
-	 
-	 @Test
-	 public void payWithDebit() throws CashOverloadException,
-	 NoCashAvailableException, DisabledException, IOException {
-	 CardIssuer ci1 = new CardIssuer(SupportedCardIssuers.ONE.getIssuer(), 1);
-	 CardIssuerDatabase.CARD_ISSUER_DATABASE.put(SupportedCardIssuers.ONE.
-	 getIssuer(), ci1);
-	 Card debitCard = new Card(SupportedCardIssuers.ONE.getIssuer(),
-	 "5299334598001547", "Brandon Chan", "666", null, false, false);
-	 
-	 Calendar exp = Calendar.getInstance();
-	 exp.set(Calendar.YEAR, 2099);
-	 exp.set(Calendar.MONTH, 12);
-	 
-	 ci1.addCardData(debitCard.number, debitCard.cardholder, exp, debitCard.cvv,
-	 10000);
-	 
-	 session.start();
-	 for(int i =0; i < 100; i++) {
-	 scs.getMainScanner().scan(item);
-	 }
-	 scs.getBaggingArea().addAnItem(item);
-	 session.payByCard();
-	 
-	 Funds funds = session.getFunds();
-	 
-	 assertEquals(SessionState.PAY_BY_CARD, session.getState());
-	 
-	 boolean read = false;
-	 while (!read) {
-	 try {
-	 scs.getCardReader().swipe(debitCard);
-	 read = true;
-	 } catch (MagneticStripeFailureException e) {
-	 }
-	 }
-	 assertEquals("Session is fully paid for", BigDecimal.ZERO,
-	 funds.getAmountDue());
-	 assertEquals("Session has been notified of full payment", session.getState(),
-	 SessionState.PRE_SESSION);
-	 }
-	 
-	 // Tests for removing an item
-	 
-	 @Test
-	 public void addThenRemoveItem() throws DisabledException,
-	 CashOverloadException {
-	 session.start();
-	 for(int i =0; i < 100; i++) {
-	 scs.getMainScanner().scan(item);
-	 }
-	 scs.getBaggingArea().addAnItem(item);
-	 for(int i =0; i < 100; i++) {
-	 scs.getMainScanner().scan(item2);
-	 }
-	 scs.getBaggingArea().addAnItem(item2);
-	 session.getManager().removeItem(product);
-	 scs.getBaggingArea().removeAnItem(item);
-	 session.payByCash();
-	 int count = 0;
-	 while(count<10) {
-	 scs.getCoinSlot().receive(dollar);
-	 if(scs.getCoinTray().collectCoins().isEmpty()) {
-	 count = count + 1;
-	 }
-	 }
-	 Funds funds = session.getFunds();
-	 assertEquals("Session is fully paid for", BigDecimal.ZERO,
-	 funds.getAmountDue());
-	 assertEquals("Session has been notified of full payment", session.getState(),
-	 SessionState.PRE_SESSION);
-	 }
-	 
+	public void payWithCredit() throws CashOverloadException,
+			NoCashAvailableException, DisabledException, IOException {
+		CardIssuer ci1 = new CardIssuer(SupportedCardIssuers.ONE.getIssuer(), 1);
+		CardIssuerDatabase.CARD_ISSUER_DATABASE.put(SupportedCardIssuers.ONE.getIssuer(), ci1);
+		Card creditCard = new Card(SupportedCardIssuers.ONE.getIssuer(),
+				"5299334598001547", "Brandon Chan", "666", null, false, false);
+
+		Calendar exp = Calendar.getInstance();
+		exp.set(Calendar.YEAR, 2099);
+		exp.set(Calendar.MONTH, 12);
+
+		ci1.addCardData(creditCard.number, creditCard.cardholder, exp,
+				creditCard.cvv, 10000);
+
+		session.start();
+		for (int i = 0; i < 100; i++) {
+			scs.getMainScanner().scan(item);
+		}
+		scs.getBaggingArea().addAnItem(item);
+		session.payByCard();
+
+		Funds funds = session.getFunds();
+
+		assertEquals(SessionState.PAY_BY_CARD, session.getState());
+
+		boolean read = false;
+		while (!read) {
+			try {
+				scs.getCardReader().swipe(creditCard);
+				read = true;
+			} catch (MagneticStripeFailureException e) {
+			}
+		}
+		assertEquals("Session is fully paid for", BigDecimal.ZERO,
+				funds.getAmountDue());
+		assertEquals("Session has been notified of full payment", session.getState(),
+				SessionState.DISABLED);
+	}
+
+	@Test
+	public void payWithDebit() throws CashOverloadException,
+			NoCashAvailableException, DisabledException, IOException {
+		CardIssuer ci1 = new CardIssuer(SupportedCardIssuers.ONE.getIssuer(), 1);
+		CardIssuerDatabase.CARD_ISSUER_DATABASE.put(SupportedCardIssuers.ONE.getIssuer(), ci1);
+		Card debitCard = new Card(SupportedCardIssuers.ONE.getIssuer(),
+				"5299334598001547", "Brandon Chan", "666", null, false, false);
+
+		Calendar exp = Calendar.getInstance();
+		exp.set(Calendar.YEAR, 2099);
+		exp.set(Calendar.MONTH, 12);
+
+		ci1.addCardData(debitCard.number, debitCard.cardholder, exp, debitCard.cvv,
+				10000);
+
+		session.start();
+		for (int i = 0; i < 100; i++) {
+			scs.getMainScanner().scan(item);
+		}
+		scs.getBaggingArea().addAnItem(item);
+		session.payByCard();
+
+		Funds funds = session.getFunds();
+
+		assertEquals(SessionState.PAY_BY_CARD, session.getState());
+
+		boolean read = false;
+		while (!read) {
+			try {
+				scs.getCardReader().swipe(debitCard);
+				read = true;
+			} catch (MagneticStripeFailureException e) {
+			}
+		}
+		assertEquals("Session is fully paid for", BigDecimal.ZERO,
+				funds.getAmountDue());
+		assertEquals("Session has been notified of full payment", session.getState(),
+				SessionState.DISABLED);
+	}
+
+	// Tests for removing an item
+
+	@Test
+	public void addThenRemoveItem() throws DisabledException,
+			CashOverloadException {
+		session.start();
+		for (int i = 0; i < 100; i++) {
+			scs.getMainScanner().scan(item);
+		}
+		scs.getBaggingArea().addAnItem(item);
+		for (int i = 0; i < 100; i++) {
+			scs.getMainScanner().scan(item2);
+		}
+		scs.getBaggingArea().addAnItem(item2);
+		session.getManager().removeItem(product);
+		scs.getBaggingArea().removeAnItem(item);
+		session.payByCash();
+		int count = 0;
+		while (count < 10) {
+			scs.getCoinSlot().receive(dollar);
+			if (scs.getCoinTray().collectCoins().isEmpty()) {
+				count = count + 1;
+			}
+		}
+		Funds funds = session.getFunds();
+		assertEquals("Session is fully paid for", BigDecimal.ZERO,
+				funds.getAmountDue());
+		assertEquals("Session has been notified of full payment", session.getState(),
+				SessionState.DISABLED);
+	}
 
 	@Test
 	public void addBulkyItem() {
@@ -549,7 +546,7 @@ public class SelfCheckoutStationSystemTest extends AbstractTest {
 	}
 
 	@Test
-	public void powerCycle(){
+	public void powerCycle() {
 		scs.getBaggingArea().turnOff();
 		scs.getScanningArea().turnOff();
 		scs.getMainScanner().turnOff();
@@ -558,7 +555,7 @@ public class SelfCheckoutStationSystemTest extends AbstractTest {
 		scs.getReusableBagDispenser().turnOff();
 		scs.getPrinter().turnOff();
 		scs.getCardReader().turnOff();
-		
+
 		scs.getBaggingArea().turnOn();
 		scs.getScanningArea().turnOn();
 		scs.getMainScanner().turnOn();
@@ -599,17 +596,16 @@ public class SelfCheckoutStationSystemTest extends AbstractTest {
 		scs.getCoinSlot().enable();
 		scs.getCoinValidator().enable();
 		scs.getCoinStorage().enable();
-		
-		for(BigDecimal denomination : scs.getBanknoteDenominations()){
+
+		for (BigDecimal denomination : scs.getBanknoteDenominations()) {
 			scs.getBanknoteDispensers().get(denomination).disable();
 			scs.getBanknoteDispensers().get(denomination).enable();
 		}
-		for(BigDecimal denomination : scs.getCoinDenominations()){
+		for (BigDecimal denomination : scs.getCoinDenominations()) {
 			scs.getCoinDispensers().get(denomination).disable();
 			scs.getCoinDispensers().get(denomination).enable();
 		}
 
-	
 		assertEquals(SessionState.PRE_SESSION, session.getState());
 	}
 
