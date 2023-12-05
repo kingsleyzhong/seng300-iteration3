e9af346e586ba318de23673b8498b31596228976
CeeRobitaille <chloe.robitaille@ucalgary.ca>
2023-12-04 20:40:41 -0700

    fixed spelling error in reciept (again) (pls review my other pull request its been hours)

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/receipt/Receipt.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/receipt/Receipt.java
@@ -95,7 +95,7 @@ public class Receipt {
 			isOutOfPaper = true;
 			duplicateNeeded = true;
 			for(ReceiptListener l : listeners) {
-				l.notifiyOutOfPaper();
+				l.notifyOutOfPaper();
 			}
 		}
 
@@ -104,7 +104,7 @@ public class Receipt {
 			isOutOfInk = true;
 			duplicateNeeded = true;
 			for(ReceiptListener l : listeners) {
-				l.notifiyOutOfInk();
+				l.notifyOutOfInk();
 			}
 		}
 
