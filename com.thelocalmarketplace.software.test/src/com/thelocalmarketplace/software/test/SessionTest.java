a2af6550d831f849ba8f0b346cac2ae6197bb147
SubegSC <subegschahal@gmail.com>
2023-12-04 22:01:27 -0700

    Increased coverage for Session tests

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SessionTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SessionTest.java
@@ -402,6 +402,22 @@ public class SessionTest extends AbstractTest {
         assertTrue(itemManager == session.getManager());
     }
     
+    @Test
+    public void sessionGetReceipt()
+    {
+        session.setup(itemManager, funds, weight, receiptPrinter, membership, scs, bagDispenser);
+
+        assertTrue(receiptPrinter == session.getReceipt());
+    }
+    
+    @Test
+    public void sessionGetMembership()
+    {
+        session.setup(itemManager, funds, weight, receiptPrinter, membership, scs, bagDispenser);
+
+        assertTrue(membership == session.getMembership());
+    }
+    
     /*
      *Sessions can only be disabled when in the pre-session state 
      */
